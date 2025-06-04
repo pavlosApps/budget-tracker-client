a8378ae7700f8cbe4dec687a4ecdef57e8b9aa44
Pavlos Pavlides <62739420+eurobank-cy@users.noreply.github.com>
2025-06-04 15:25:22 +0300

    link import

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import API from '../api';
+import { Link } from 'react-router-dom'; // make sure this is at the top
 
 export default function Dashboard() {
   const [transactions, setTransactions] = useState([]);
